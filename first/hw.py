96166bf1ac917211f6ae8e7310f5a92689f9be6a
DaniilPyankov <dpyankov@icloud.com>
2021-11-08 21:55:11 +0500

    Удалил 0 в скобках

--- first/hw.py
+++ first/hw.py
@@ -1,9 +1,9 @@
 # coding: utf
 # Поиск минимального элемента из списка
 A = [3, 4, 2, 1, 0, 8, 9, -1, 100, 0]
-Min = A.pop(0)
+Min = A.pop()
 while A:
-    a = A.pop(0)
+    a = A.pop()
     if a < Min:
         Min = a
 print(Min)
