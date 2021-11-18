16d82e1146dbabd1461b2e809455541317ca551a
DaniilPyankov <dpyankov@icloud.com>
2021-11-19 02:07:45 +0500

    Описание персоны списком/словарем.

---
+++ lesson_2.py
@@ -0,0 +1,12 @@
+# lesson 2
+
+# list:
+y = ['Ivan Semenov Ivanovitch', 1990, 180, 85]
+print(y)
+
+# dict:
+x = {('Ivan', 'Semenov', 'Semenovitch'):{'gender':'man',
+                          'births':1990,
+                          'height':180,
+                          'weight':85}}
+print(x)
