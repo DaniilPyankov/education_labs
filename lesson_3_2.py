99063087cc8df778396c33341cd5bb3bb5dfaf55
DaniilPyankov <dpyankov@icloud.com>
2021-12-14 21:06:19 +0500

    Поиск по критерию в словаре

--- lesson_3_2.py
+++ lesson_3_2.py
@@ -1,36 +1,42 @@
 # lesson 3
 
-# Dict find:
+# ввод данных
 people = {
-    'Ivan Semenov': {'sex':'m', 'beth':1990, 180, 85},
-    'Petr Popov': {'m', 1980, 176, 89},
-    'Tatyana Ivanova': {'w', 1987, 165, 54},
-    'Dmitriy Petrov': {'m', 1981, 200, 110},
-    'Olga Olgina': {'w', 1970, 174, 65}
+    'Ivan Semenov': {'г.р.': 1990, 'рост': 180, 'вес': 130},
+    'Petr Popov': {'г.р.': 1980, 'рост': 176, 'вес': 89},
+    'Tatyana Ivanova': {'г.р.': 1987, 'рост': 165, 'вес': 54},
+    'Dmitriy Petrov': {'г.р.': 1981, 'рост': 200, 'вес': 105},
+    'Olga Olgina': {'г.р.': 1970, 'рост': 174, 'вес': 65}
 }
 
+# вывод на печать списка
 print('Список людей:')
-for n in range(len(people)):
-    print(n+1, end='    ')
-    print(', '.join(str(i) for i in people[n]))
+n = 0
+for key, value in people.items():
+    n += 1
+    print(n, key, value, sep='  ')
 
-# Критерий выбора персоны:
-max = ['Самый младший', 'Самый высокий', 'Самый тяжелый']
-print('')
-print('Введите критерий поиска персоны:')
-print (f'    {max[0]} -  "1"')
-print (f'    {max[1]} -  "2"')
-print (f'    {max[2]} -  "3"')
-ind = int(input()) + 1
-if ind < 2 or ind > 4:
-    print('Такого критерия нет!')
+# ввод критерия выбора
+krit = [0, 'Самый старший', 'Самый высокий', 'Самый тяжелый']
+i = int(input(f'Введите критерий ({krit[1]} - 1, {krit[2]} - 2, {krit[3]} - 3):'))
+if i < 1 or i > 3:
+    print('Неверный ввод критерия выбора')
 
-ind_max = 0
-pers_max = []
-for pers in people:
-    if pers[ind] > ind_max:
-        ind_max = pers[ind]
-        pers_max = pers
+# сравнение элементов словаря по критерию
+ind = {1: 'г.р.', 2: 'рост', 3: 'вес'}
+
+if i == 1:
+    ind_m = 3000
+    for pers, info in people.items():
+        if info[ind[i]] < ind_m:
+            ind_m = info[ind[i]]
+            pers_m = pers
+else:
+    ind_m = 0
+    for pers, info in people.items():
+        if info[ind[i]] > ind_m:
+            ind_m = info[ind[i]]
+            pers_m = pers
 
 print(' ')
-print(max[ind-2], pers_max[0], ':  ', ind_max)
+print(f'{krit[i]} в списке {pers_m}. Его {ind[i]} {ind_m}')
